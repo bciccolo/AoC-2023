793f9ee9819c9d0c0c2f3bd7b736b1000100687e
Brian Ciccolo <brian@passingfocus.net>
2023-12-05 07:35:13 -0500

    Part 2 works for snippet only The full data set simply has TOO MANY seeds for this approach. VS Code kills the Python process before it can complete.

--- day5.py
+++ day5.py
@@ -17,6 +17,43 @@ def convert_source_to_destination(value, lookup_map):
     return value
 
 
+def expand_seeds(seeds):
+    expanded = { seeds[0] }
+
+    for i in range(0, len(seeds), 2):
+        start = seeds[i]
+        length = seeds[i + 1]
+        for j in range(length):
+            expanded.add(start + j)
+
+    return expanded
+
+
+def find_min_location(seeds):
+    min_location = -1
+    for seed in seeds:
+        soil = convert_source_to_destination(seed, seed_to_soil)
+        fertilizer = convert_source_to_destination(soil, soil_to_fertilizer)
+        water = convert_source_to_destination(fertilizer, fertilizer_to_water)
+        light = convert_source_to_destination(water, water_to_light)
+        temperature = convert_source_to_destination(light, light_to_temperature)
+        humidity = convert_source_to_destination(temperature, temperature_to_humidity)
+        location = convert_source_to_destination(humidity, humidity_to_location)
+        if min_location == -1 or location < min_location:
+            min_location = location
+
+        # print('Seed ' + str(seed) + ', ' +
+        #       'soil ' + str(soil) + ', ' +
+        #       'fertilizer ' + str(fertilizer) + ', ' +
+        #       'water ' + str(water) + ', ' +
+        #       'light ' + str(light) + ', ' +
+        #       'temperature ' + str(temperature) + ', ' +
+        #       'humidity ' + str(humidity) + ', ' +
+        #       'location ' + str(location))
+
+    return min_location
+
+
 def load_data(file):
     global seeds
 
@@ -56,32 +93,7 @@ def load_data(file):
             elif blank_counter == 7:
                 humidity_to_location.append(range)
 
-
-def part_1():
-    min_location = -1
-    for seed in seeds:
-        soil = convert_source_to_destination(seed, seed_to_soil)
-        fertilizer = convert_source_to_destination(soil, soil_to_fertilizer)
-        water = convert_source_to_destination(fertilizer, fertilizer_to_water)
-        light = convert_source_to_destination(water, water_to_light)
-        temperature = convert_source_to_destination(light, light_to_temperature)
-        humidity = convert_source_to_destination(temperature, temperature_to_humidity)
-        location = convert_source_to_destination(humidity, humidity_to_location)
-        if min_location == -1 or location < min_location:
-            min_location = location
-
-        # print('Seed ' + str(seed) + ', ' +
-        #       'soil ' + str(soil) + ', ' +
-        #       'fertilizer ' + str(fertilizer) + ', ' +
-        #       'water ' + str(water) + ', ' +
-        #       'light ' + str(light) + ', ' +
-        #       'temperature ' + str(temperature) + ', ' +
-        #       'humidity ' + str(humidity) + ', ' +
-        #       'location ' + str(location))
-
-    print('Part 1: ' + str(min_location))
-
-load_data('day5.dat')
+load_data('day5-snippet.dat')
 # print(seeds)
 # print(seed_to_soil)
 # print(soil_to_fertilizer)
@@ -91,4 +103,5 @@ load_data('day5.dat')
 # print(temperature_to_humidity)
 # print(humidity_to_location)
 
-part_1()
+print('Part 1: ' + str(find_min_location(seeds)))
+print('Part 2: ' + str(find_min_location(expand_seeds(seeds))))
