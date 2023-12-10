a251f9a4c0482c61202c942bf3f4efd3639dce66
Brian Ciccolo <brian@passingfocus.net>
2023-12-10 14:34:43 -0500

    Solved part 2

--- day10.py
+++ day10.py
@@ -10,6 +10,7 @@ Y = 0
 
 maze = []
 start = []
+start_pipe = ''
 
 def load_maze(filename):
     global start
@@ -17,6 +18,11 @@ def load_maze(filename):
     file = open(filename, 'r')
     for row, line in enumerate(file.readlines()):
         line = line.strip()
+
+        # Snippet file contains multiple mazes separated by blank lines
+        if line == '':
+            break
+
         maze.append([])
 
         for col, char in enumerate(line):
@@ -29,25 +35,76 @@ def load_maze(filename):
 
 
 def load_waypoints():
+    global start_pipe
+
     waypoints = []
+
+    up = False
+    left = False
+    down = False
+    right = False
 
     move = move_up(start)
     if move:
         waypoints.append(move)
+        up = True
 
     move = move_left(start)
     if move:
         waypoints.append(move)
+        left = True
 
     move = move_down(start)
     if move:
         waypoints.append(move)
+        down = True
 
     move = move_right(start)
     if move:
         waypoints.append(move)
+        right = True
+
+    if up and down:
+        start_pipe = '|'
+    elif left and right:
+        start_pipe = '-'
+    elif up and right:
+        start_pipe = 'L'
+    elif up and left:
+        start_pipe = 'J'
+    elif down and right:
+        start_pipe = 'F'
+    elif down and left:
+        start_pipe = '7'
 
     return waypoints
+
+
+def mark_points(grid):
+    for row in range(len(grid)):
+        outside = True
+        last = ''
+        for col in range(len(grid[row])):
+            if grid[row][col] == '.':
+                if outside:
+                    grid[row][col] = 'O'
+                else:
+                    grid[row][col] = 'I'
+            elif grid[row][col] == '|':
+                outside = not outside
+                last = ''
+            elif grid[row][col] == 'L':
+                last = 'L'
+            elif grid[row][col] == 'F':
+                last = 'F'
+            elif grid[row][col] == 'J':
+                if last == 'F':
+                    outside = not outside
+                last = ''
+            elif grid[row][col] == '7':
+                if last == 'L':
+                    outside = not outside
+                last = ''
 
 
 def move_along_path(waypoint):
@@ -124,6 +181,7 @@ def move_up(point):
 load_maze('day10.dat')
 
 waypoint_1, waypoint_2 = load_waypoints()
+full_path = [start, waypoint_1['point'], waypoint_2['point']]
 
 steps = 1
 # print(steps)
@@ -132,10 +190,37 @@ steps = 1
 while not waypoint_1['point'] == waypoint_2['point']:
     waypoint_1 = move_along_path(waypoint_1)
     waypoint_2 = move_along_path(waypoint_2)
+
+    full_path.append(waypoint_1['point'])
+    full_path.append(waypoint_2['point'])
+
     steps += 1
     # print(steps)
     # pprint(waypoint_1)
     # pprint(waypoint_2)
 
 print('Part 1: ' + str(steps))
-print('Part 2: ' + str(0))
+
+grid = []
+for i in range(len(maze)):
+    grid.append(list('.' * (len(maze[i]))))
+
+for point in full_path:
+    grid[point[Y]][point[X]] = maze[point[Y]][point[X]]
+
+# for row in grid:
+#     print("".join(row))
+
+# print('Start pipe: ' + start_pipe)
+grid[start[Y]][start[X]] = start_pipe
+
+mark_points(grid)
+
+# for row in grid:
+#     print("".join(row))
+
+inside = 0
+for row in grid:
+    inside += row.count('I')
+
+print('Part 2: ' + str(inside))
