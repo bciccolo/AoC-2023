474c4e27779b49e4b3018a3e768c75a8c725d947
Brian Ciccolo <brian@passingfocus.net>
2023-12-11 12:46:49 -0500

    Simplified code to calculate both parts at once

--- day11.py
+++ day11.py
@@ -3,19 +3,20 @@ Y = 0
 
 expansion_columns = []
 expansion_rows = []
+galaxies = []
+universe = []
 
-def find_galaxies(universe):
-    galaxies = []
+def get_total_distance(expansion_factor):
+    total_distance = 0
 
-    for y in range(len(universe)):
-        for x in range(len(universe[y])):
-            if universe[y][x] == '#':
-                galaxies.append([y, x])
+    for i in range(len(galaxies)):
+        for j in range(i + 1, len(galaxies)):
+            total_distance += get_distance(galaxies[i], galaxies[j], expansion_factor)
 
-    return galaxies
+    return total_distance
 
 
-def get_distance(point_1, point_2):
+def get_distance(point_1, point_2, expansion_factor):
     distance = abs(point_1[X] - point_2[X]) + abs(point_1[Y] - point_2[Y])
 
     # Check expansion rows
@@ -23,27 +24,32 @@ def get_distance(point_1, point_2):
     max_y = max(point_1[Y], point_2[Y])
     for row in expansion_rows:
         if row > min_y and row < max_y:
-            distance += 1_000_000 - 1
+            distance += expansion_factor
 
     # Check expansion columns
     min_x = min(point_1[X], point_2[X])
     max_x = max(point_1[X], point_2[X])
     for col in expansion_columns:
         if col > min_x and col < max_x:
-            distance += 1_000_000 - 1
+            distance += expansion_factor
 
     return distance
 
 
-def load_universe(file_name):
-    universe = []
+def load_galaxies():
+    for y in range(len(universe)):
+        for x in range(len(universe[y])):
+            if universe[y][x] == '#':
+                galaxies.append([y, x])
 
+
+def load_universe(file_name):
     count = 0
     file = open(file_name, 'r')
     for line in file.readlines():
         line = line.strip()
 
-        # Expand rows (easy)
+        # Find row expansions (easy)
         if line.count('#') == 0:
             # universe.append(list(line))
             expansion_rows.append(count)
@@ -52,35 +58,19 @@ def load_universe(file_name):
 
         count += 1
 
-    # Expand columns (not so easy)
+    # Find column expansions (not as easy)
     for i in range(len(universe[0])):
         column = [row[i] for row in universe]
         if column.count('#') == 0:
             expansion_columns.append(i)
-    # i = 0
-    # while i < len(universe[0]):
-    #     column = [row[i] for row in universe]
-    #     if column.count('#') == 0:
-    #         for row in universe:
-    #             row.insert(i, '.')
-    #         i += 1
-
-    #     i += 1
-
-    return universe
 
 
-universe = load_universe('day11.dat')
-galaxies = find_galaxies(universe)
+load_universe('day11.dat')
+load_galaxies()
 
 # for row in universe:
 #     print("".join(row))
 # print(galaxies)
 
-total_distance = 0
-for i in range(len(galaxies)):
-    for j in range(i + 1, len(galaxies)):
-        total_distance += get_distance(galaxies[i], galaxies[j])
-
-print('Part 1: ' + str(total_distance))
-print('Part 2: ' + str(0))
+print('Part 1: ' + str(get_total_distance(1)))
+print('Part 2: ' + str(get_total_distance(999_999)))
