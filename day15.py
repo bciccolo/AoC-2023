d1b23b972305f93957a46261f1859d2c0ef5690a
Brian Ciccolo <brian@passingfocus.net>
2023-12-15 08:22:41 -0500

    Solved part 2

--- day15.py
+++ day15.py
@@ -21,3 +21,40 @@ file = open('day15.dat', 'r')
 line = file.readlines()[0].strip()
 
 print('Part 1: ' + str(calculate_hash(line)))
+
+boxes = []
+for i in range(256):
+    boxes.append([])
+
+for instruction in line.split(','):
+    operator = '='
+    if operator not in instruction:
+        operator = '-'
+    label, focal_length = instruction.split(operator)
+    box_index = calculate_hash(label)
+    # print(instruction + ' >> label: ' + label + ' hash: ' + str(value) + ' lens: ' + lens)
+
+    slots = boxes[box_index]
+    slot_index = -1
+    for i in range(len(slots)):
+        if slots[i][0] == label:
+            slot_index = i
+            break
+
+    if operator == '=':
+        if slot_index == -1:
+            slots.append([label, focal_length])
+        else:
+            slots[slot_index][1] = focal_length
+    else:
+        if slot_index > -1:
+            slots.pop(slot_index)
+
+focusing_power = 0
+for i, box in enumerate(boxes):
+    if len(box) > 0:
+        # print('Box ' + str(i) + ': ' + str(box))
+        for j, slot in enumerate(box):
+            focusing_power += (i + 1) * (j + 1) * int(slot[1])
+
+print('Part 2: ' + str(focusing_power))
