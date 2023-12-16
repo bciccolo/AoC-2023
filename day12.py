d55e378f9a460ef79f48c82d0e129230b1db782f
Brian Ciccolo <brian@passingfocus.net>
2023-12-15 22:53:47 -0500

    Removed old debugging code

--- day12.py
+++ day12.py
@@ -1,15 +1,10 @@
 def count_patterns(pattern, groups, start_pattern_index, start_group_index):
-    global cache, cache_hits, line_number
+    global cache_hits
 
-    simplified = '.'.join([y for y in pattern[start_pattern_index:].split('.') if len(y) > 0])
+    # Note: We have to join the groups with something other than the empty string
+    # because otherwise lists like [1,1] and [11] will result in the same string.
     params = pattern[start_pattern_index:] + '|'.join([str(size) for size in groups[start_group_index:]])
-    # print(params)
-    # if params == '#??.?#?????????1111':
-    #     print('culprit at line: ' + str(line_number))
-
     if params in cache:
-        # if line_number == 490:
-        #     print(params + ' >>> ' + str(cache[params]))
         cache_hits += 1
         return cache[params]
 
@@ -49,8 +44,6 @@ def count_patterns(pattern, groups, start_pattern_index, start_group_index):
             if start > 0 and pattern[start - 1] == '#':
                 possible = False
 
-            # HERE - necessary check?
-            # if start_group_index < len(groups) - 1:
             if i + 1 < len(pattern) and pattern[i + 1] == '#':
                 possible = False
 
@@ -63,7 +56,6 @@ def count_patterns(pattern, groups, start_pattern_index, start_group_index):
         if possible:
             if start_group_index == len(groups) - 1:
                 count += 1
-                # print(pattern[:start].replace('?', '.') + ('#' * group) + '.' + pattern[start + group + 1:])
             else:
                 updated = pattern[:start].replace('?', '.') + ('#' * group) + '.' + pattern[start + group + 1:]
                 count += count_patterns(updated, groups, i + 2, start_group_index + 1)
@@ -73,18 +65,12 @@ def count_patterns(pattern, groups, start_pattern_index, start_group_index):
     return count
 
 
-line_number = 1
 def solve(unfold):
-    global line_number
-
     total = 0
 
     file = open('day12.dat', 'r')
     for line in file.readlines():
         parts = line.split()
-
-        if line.strip() == '':
-            break
 
         pattern = parts[0]
         if (unfold > 1):
@@ -100,23 +86,11 @@ def solve(unfold):
         count = count_patterns(pattern, groups, 0, 0)
         total += count
 
-        # if line_number == 490:
-        #     print('Line: ' + str(line_number) + ': ' + str(count))
-        line_number += 1
-
     return total
 
 
-import time
-
 cache = {}
 cache_hits = 0
-
-print('Part 1:   ' + str(solve(1)))
-begin = time.time()
-print('Part 2:   ' + str(solve(5)))
-# 6512849116431 - too low,
-# 6512849164175 - not right
-# 6512849198636
-print(str(time.time() - begin) + " seconds")
+print('Part 1: ' + str(solve(1))) # 7191
+print('Part 2: ' + str(solve(5))) # 6512849198636
 print('Cache hits: ' + str(cache_hits))
